87c2c603c1b0a4067c5c2e077adb78a98298de06
Darcy Atlas <foreverdiffrent@live.com>
2019-12-20 16:10:58 -0800

    signup styled

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -18,7 +18,7 @@ function Footer(){
                 <Col className="text-center">  
                     <p className="name">Bill Coury</p>    
                     <a href="https://billcoury.github.io/updatedbio/index.html" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faDesktop} className="social"/></a>
-                    <a href="https://www.linkedin.com/in/stephanie-lake-43b89140/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} className="social"/></a>
+                    <a href="https://www.linkedin.com/in/bill-coury-a17162174/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} className="social"/></a>
                     <a href="https://github.com/billcoury" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} className="social"/></a> 
                 </Col>   
                 <Col className="text-center">  
