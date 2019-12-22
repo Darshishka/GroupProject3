3af11787c52caa603b23f8e2f63417e5a1598f4f
Darcy Atlas <foreverdiffrent@live.com>
2019-12-21 20:33:10 -0800

    show posts and sidebar/filters done for now

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -3,8 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import "./index.css";
 import NavTab from "../components/navtab";
 import Sidenav from "../components/sidenav";
-import AllPosts from "../components/allPosts";
-import Post from "../components/post";
+import AllPosts from "../components/showPosts";
+import Post from "../components/comment";
 //I did not do a very good job getting the sidenav and posts to align nicely, just threw it together. There's probably a much better way
 function Main(){
     return(
