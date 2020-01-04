33337ff3e71c604989bdd26506eaba8019be646c
Whitney Tarkington <whittark@gmail.com>
2020-01-04 11:57:07 -0800

    login styling

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -3,14 +3,18 @@ import { Container } from "react-bootstrap";
 import "./index.css";
 import Signin from "../components/signin/index";
 import Signup from "../components/signup";
+import Logo from "../components/signin/favorlyHands.png";
 
 function Home(){
     return (
         <div id="homeContainer">
             <Container className="text-center">
                 <div className="heading">
-                    <h2>Welcome to Favorly!</h2>
-                    <h5>A place where you can create groups and help others</h5>
+                    <img src={Logo}></img>
+                    <h2>FAVORLY</h2>
+                    <hr></hr>
+                    <h5>A message board to</h5>
+                    <h5>get or give a helping hand</h5>
                 </div>                
             </Container>           
                 <Signin />
