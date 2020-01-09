321a0020599520246aca54a776cbdc87b83097cf
Darcy Atlas <foreverdiffrent@live.com>
2020-01-08 20:27:32 -0800

    done

--- client/src/components/showPosts/index.js
+++ client/src/components/showPosts/index.js
@@ -6,7 +6,17 @@ import { SHOWPOST, SHOWCOMMENT, POSTID } from "../../actions";
 import API from "../../utils/API";
 import Comment from "../comment";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDesktop } from "@fortawesome/free-solid-svg-icons";
+import { faCarAlt } from "@fortawesome/free-solid-svg-icons";
+import { faBaby } from "@fortawesome/free-solid-svg-icons";
+import { faSchool } from "@fortawesome/free-solid-svg-icons";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faPaw } from "@fortawesome/free-solid-svg-icons";
+import { faTools } from "@fortawesome/free-solid-svg-icons";
+import { faTree } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
+import { faHandHoldingHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHandHolding } from "@fortawesome/free-solid-svg-icons";
 
 function Posts(){
     const [posts, setPosts] = useState([]);
@@ -51,6 +61,21 @@ function Posts(){
                     posts.map((el, i) => (
                         <Card key={el.id}>
                             <Accordion.Toggle as={Card.Header} eventKey={i}>
+                                <Row id="title">
+                                    <Col id="type titleIcon">{el.type === "offer" ? (<FontAwesomeIcon icon={faHandHoldingHeart} className="type fa-lg" id="offer"/>) : <FontAwesomeIcon icon={faHandHolding} className="type fa-lg" id="request"/>}</Col>
+                                    <Col id="category">
+                                        {el.category === "automotive" ? (<FontAwesomeIcon icon={faCarAlt} className="category fa-lg" id="automotive"/>) : 
+                                        el.category === "child" ? (<FontAwesomeIcon icon={faBaby} className="category fa-lg" id="childcare"/>) : 
+                                        el.category === "education" ? (<FontAwesomeIcon icon={faSchool} className="category fa-lg" id="education"/>) : 
+                                        el.category === "home" ? (<FontAwesomeIcon icon={faHome} className="category fa-lg" id="home"/>) : 
+                                        el.category === "pet" ? (<FontAwesomeIcon icon={faPaw} className="category fa-lg" id="pet"/>) : 
+                                        el.category === "tools" ? (<FontAwesomeIcon icon={faTools} className="category fa-lg" id="tools"/>) :
+                                        el.category === "yard" ? (<FontAwesomeIcon icon={faTree} className="category fa-lg" id="yard"/>) :
+                                        el.category === "errands" ? (<FontAwesomeIcon icon={faShoppingCart} className="category fa-lg" id="errands"/>) :
+                                        <FontAwesomeIcon icon={faQuestionCircle} className="category fa-lg" id="other"/>
+                                        }
+                                    </Col>
+                                </Row>
                                 <Row>
                                     <Row id="title">
                                         <Col id="subject">
@@ -60,10 +85,6 @@ function Posts(){
                                     <Col className="text-right">{el.firstName} {el.lastName}</Col>
                                     <Col className="text-right"><Button id={el.id} onClick={showComment}>Comment</Button></Col>
                                     <Comment />
-                                </Row>
-                                <Row id="title">
-                                    <Col id="type">{el.type === "post" ? (<FontAwesomeIcon icon={faDesktop} className="type"/>) : <FontAwesomeIcon icon={faDesktop} className="type"/>}</Col>
-                                    <Col id="category">{el.category === "automotive" ? (<FontAwesomeIcon icon={faDesktop} className="category"/>) : el.category === "child" ? (<FontAwesomeIcon icon={faDesktop} className="category"/>) : el.category === "education" ? (<FontAwesomeIcon icon={faDesktop} className="category"/>) : <FontAwesomeIcon icon={faDesktop} className="category"/>}</Col>
                                 </Row>
                                 <Row>
                                     <Col>{el.message}</Col>
