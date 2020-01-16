ddcc9d62b18990e1782b17b15da2430c0e2c8dc6
sjconst <stephanie.c.lake@gmail.com>
2020-01-15 19:56:20 -0800

    fixed sidenav with columns in main

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import "./index.css";
 import NavTab from "../components/navtab";
 import Sidenav from "../components/sidenav";
@@ -9,12 +9,18 @@ import Posts from "../components/showPosts";
 function Main(){
     return(
         <div id="mainContainer">
-            <NavTab />
-            <Sidenav />
-            <AddPost />
-            <Container id="pageContainer">   
-                <Posts />
-            </Container>
+            <NavTab />            
+            <Row>
+                <Col xs={3}>
+                    <Sidenav />
+                </Col>
+                <Col>
+                    <AddPost />
+                    <Container id="pageContainer">   
+                        <Posts />
+                    </Container>
+                </Col>
+            </Row>
         </div>
     )
 }
